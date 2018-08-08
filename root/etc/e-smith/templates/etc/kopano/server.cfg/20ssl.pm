e52acb324d28a9b07b4b76c82abfd62b3b345bcf
Mark Verlinde <mark@havak.nl>
2018-08-08 20:40:01 +0200

    Re-enable SSL

--- root/etc/e-smith/templates/etc/kopano/server.cfg/20ssl.pm
+++ root/etc/e-smith/templates/etc/kopano/server.cfg/20ssl.pm
@@ -2,7 +2,6 @@
 #
 #  20 SSL SETTINGS
 #
-#  no SSL for konapo server in this demo, see TODO below
 
 # TODO :
 #  - FIX: SSL for kopano-server does not work with NSRV.crt, 
@@ -12,8 +11,8 @@
 # https://github.com/NethServer/nethserver-httpd-admin/commit/fecfa73e283fa1c93ba2bed5efd3c29e08c57498
 #
 
-#server_ssl_key_file = /etc/pki/kopano/certs/kopano.pem
-#server_ssl_ca_file = /etc/pki/kopano/certs/kopano.crt
-#server_ssl_protocols = !SSLv2 !SSLv3 !TLSv1 !TLSv1.1 TLSv1.2
-#server_ssl_ciphers = EDH+aRSA+AES256:EECDH+aRSA+AES256:ECDHE-ECDSA-AES256-GCM-SHA384:+ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:+ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256
-#server_ssl_prefer_server_ciphers = yes
+server_ssl_key_file = /etc/pki/kopano/certs/kopano.pem
+server_ssl_ca_file = /etc/pki/kopano/certs/kopano.crt
+server_ssl_protocols = !SSLv2 !SSLv3 !TLSv1 !TLSv1.1 TLSv1.2
+server_ssl_ciphers = EDH+aRSA+AES256:EECDH+aRSA+AES256:ECDHE-ECDSA-AES256-GCM-SHA384:+ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:+ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256
+server_ssl_prefer_server_ciphers = yes
